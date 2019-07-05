fb1f88187c6166c2df1d9a60b4f77b6928023bf1
hatsuyuki280 <yuki@mail.yuki233.com>
2019-07-05 22:25:55 +0900

    这样大概就能用pipenv了？

--- init.sh
+++ init.sh
@@ -5,6 +5,9 @@ sudo su - || su -
 
 apt install git python3 python3-pip
 
-pip3 install --upgrade pip setuptools
+pip3 install --upgrade pip3 setuptools
+pip3 install pipenv
+pipenv update
+
 
 virtualenv -p $(python3 -V | grep -o 3..) .env
--- install.sh
+++ install.sh
@@ -2,16 +2,15 @@
 ## install
 sudo su - || su -
 
-apt install git python3 python3-pip python3-bs4 python3-parse python3-lxml python3-flask python3-dev python3-uwsgi -y 1&2> /dev/none
+apt install git python3 python3-pip python3-dev -y 1&2> /dev/none
 
-pip3 install bs4 parse lxml flask uwsgi
+pip3 install --upgrade pip3 setuptools
+pip3 install pipenv
 
-pip3 install --upgrade virtualenv
-pip3 install --upgrade pip setuptools
+pipenv update
+pipenv install bs4 parse lxml flask uwsgi
 
 mkdir -p /yuki/soft/NekoSearch && cd /yuki/soft/NekoSearch
-git clone https://github.com/hatsuyuki280/TakaraSagashi.git
-
-virtualenv -p $(python3 -V | grep -o 3..) .env
+wget 
 
 cd surugayaparser
