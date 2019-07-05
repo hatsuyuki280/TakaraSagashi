517ec65b12e7e95f62cb56c48c338dea1fbf5c40
hatsuyuki280 <yuki@mail.yuki233.com>
2019-07-06 01:37:37 +0900

    错别字。。。。

--- init.sh
+++ init.sh
@@ -5,6 +5,6 @@ sudo su - || su -
 
 apt install git python3 python3-pip nginx -y 1&2> /dev/none
 
-pip3 install --upgrade pip3 setuptools
+pip3 install --upgrade pip setuptools
 pip3 install pipenv
 pipenv update
