cb003f7a2914eb5bf9e6cbad8fd126679d0ec1e4
hatsuyuki280 <yuki@mail.yuki233.com>
2019-07-05 21:56:13 +0900

    嗯。。。。

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 用于在几家在线商城进行搜索的小工具。目前打算做成api的样子
 
 # 安装  
-克隆整个存储库或者下载并运行 [install.sh](https://github.com/hatsuyuki280/TakaraSagashi/blob/master/install.sh)
+克隆整个存储库并运行[init.sh]
+或者下载并运行[install.sh](https://github.com/hatsuyuki280/TakaraSagashi/blob/master/install.sh)
+
 完全不知道怎么把依赖写进依赖文件里。。。所以先自己写一个一键安装脚本好了。。。。
---
+++ init.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+## init
+sudo su - || su -
+
+apt install git python3 pip3 python pip python-bs4 python3-bs4 python-parse python3-parse \
+            python-lxml python3-lxml python-flask python3-flask python3-dev python-dev \
+            python-uwsgi python3-uwsgi \
+            -y 1&2> /dev/none
+
+pip install bs4 parse lxml flask uwsgi
+pip3 install bs4 parse lxml flask uwsgi
+
+pip install --upgrade virtualenv
+pip3 install --upgrade virtualenv
+pip install --upgrade pip setuptools
+pip3 install --upgrade pip setuptools
+
+virtualenv -p $(python3 -V | grep -o 3..) .env
