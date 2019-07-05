3cab4044a1aade30efdefa5e85dcd5ce331e8808
hatsuyuki280 <yuki@mail.yuki233.com>
2019-07-05 21:36:01 +0900

    引入了Python3.7的虚拟环境

---
+++ JHWS.py
@@ -0,0 +1,6 @@
+from surugayaparser import SurugayaParser
+instance = SurugayaParser()
+getparam = "category=&search_word=nekopara&adult_s=1&inStock=On&grid=t&rankBy=modificationTime%3Adescending&"
+instance.set_target_url(getparam)
+items = instance.request_items(pages=1)
+print(items)
